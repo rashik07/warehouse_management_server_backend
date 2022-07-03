a910a3260dbcf5decab92b8d29ca65ee1fde6131
rashik07 <saif67090707@gmail.com>
2022-07-03 23:14:24 +0600

    get product

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pfpno.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection= client.db('headphone-inventory').collection('products');
+        app.get('/products', async (req,res)=>{
+            const query={};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+        app.get('/products/:id', async (req,res)=>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+
+            const products = await productCollection.findOne(query);
+            res.send(products);
+        });
+
+        //POST
+
+        app.post('/products', async (req,res)=>{
+
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+           
+        });
+
+        
+
+
+    }
+    finally{
+
+    }
+
+}
+
+run().catch(console.dir);
+
+
+app.get('/',(req,res)=>{
+    res.send('running');
+})
+
+
+app.listen(port, ()=>{
+    console.log('listening');
+})
